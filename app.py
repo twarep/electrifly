6dfb8a51c59cb7a071c2e7930996b4186a1ecd28
Joanna Yang <60066483+joyang15@users.noreply.github.com>
2023-07-14 15:42:16 -0400

    switched to zephyr theme

--- app.py
+++ app.py
@@ -2,35 +2,14 @@ from shiny import App, render, ui, Inputs, Outputs, Session, reactive
 from shiny.types import NavSetArg
 from typing import List
 from htmltools import css
+import shinyswatch
 
-app_ui = ui.page_fluid(
-    ui.tags.style(
-        """
-        .nav-pills {
-            background: rgba(130, 189, 245, 0.8);
-        }
-        .nav-pills .nav-link{
-            color: black;
-        }
-        .nav-pills .nav-link.active{
-            background: rgba(39, 99, 168, 0.8);
-            color: white;
-        }
-        .nav-pills a:hover {
-            background: rgba(130, 189, 245, 0.8);
-            color: white; 
-        }
-        """
-    ),
-    ui.navset_pill(
-        
-        ui.nav("Upload Data",
-               "upload data content"),
-        ui.nav("Data Analysis", 
-               "data analysis content"),
-        ui.nav("Reccomendations", 
-               "reccomendations content"),
-    ),
+app_ui = ui.page_navbar(
+    shinyswatch.theme.zephyr(),
+    ui.nav("Upload Data","upload data content"),
+    ui.nav("Data Analysis", "data analysis content"),
+    ui.nav("Reccomendations", "reccomendations content"),
+    title="Electrifly UI",
     
 )
 
