1ae5a171263a2959f87aeac8ab1b5381a52ce0b2
Meenakshi Andoorveedu <m2andoor@uwaterloo.ca>
2023-07-26 19:47:06 -0400

    added the nav tabs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from shiny import App, render, ui, Inputs, Outputs, Session, reactive
-from htmltools import HTML, div
+from htmltools import HTML, css, div
 from shiny.types import NavSetArg
 from typing import List
 from htmltools import css
@@ -13,6 +13,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 import shiny.experimental as x
+from shiny import App, Inputs, Outputs, Session, render, ui
 mypath = "./test_data/"
 
 # Initialize the flight data dates as name keys
@@ -47,6 +48,7 @@ plane_data_df = pd.DataFrame(
                  "Requested Torque": [0, 857, 758, 385]})
 
 app_ui = ui.page_navbar(
+    # {"style": "color: blue"},
     shinyswatch.theme.zephyr(),
     ui.nav("Upload Data",
             ui.download_button("downloadData", "Flight & Weather Data Refresh", style="background-color: #007bff; color: white;"),
@@ -61,32 +63,44 @@ app_ui = ui.page_navbar(
             div(HTML("<hr>")),
         ),
     ui.nav("Data Analysis", 
-            ui.include_css("bootstrap.css"),
+        ui.include_css("bootstrap.css"),
             x.ui.card(
                 x.ui.card_header("Welcome to ElectriFly's Data Analytics Interface!"),
                 x.ui.card_body("Unlock the power of your data with our intuitive and powerful user interface designed specifically for data analytics. Our platform empowers you to transform raw data into actionable insights, enabling you to make informed decisions and drive your business forward.")
                 ),
-            div("SOC vs. Time Across Multiple Flights"), 
-            ui.layout_sidebar(
-                ui.panel_sidebar(
-                    ui.input_select(
-                        "state",
-                        "Choose flight date(s):",
-                        data_file_changed_names,
-                        selected=data_file_changed_names[0],
-                        multiple=True,
+            #This creates the tabs between the recommended graph screen and the insights
+        ui.navset_tab(
+            
+            # {"style": "background-colour: rgba(0, 128, 255, 0.1)"},
+            # ui.div(
+            #     {"class": "navset-tab;"},
+            # ),
+            ui.nav("Recommended Graphs", 
+                    div("SOC vs. Time Across Multiple Flights"), 
+                    ui.layout_sidebar(
+                        ui.panel_sidebar(
+                            ui.input_select(
+                                "state",
+                                "Choose flight date(s):",
+                                data_file_changed_names,
+                                selected=data_file_changed_names[0],
+                                multiple=True,
+                            ),
+                            div(HTML("<p>To select multiple dates on <b>Windows</b>: </p>")),
+                            div("1. Press `ctrl` + select the dates"),
+                            div(HTML("<hr>")),
+                            div(HTML("<p>To select multiple dates on <b>Mac</b>: </p>")),
+                            div("1. Press `cmd` + select the dates"),
+                            width = 3
+                        ),
+                        ui.panel_main(
+                            ui.output_plot("interactive")
+                        ),
                     ),
-                    div(HTML("<p>To select multiple dates on <b>Windows</b>: </p>")),
-                    div("1. Press `ctrl` + select the dates"),
-                    div(HTML("<hr>")),
-                    div(HTML("<p>To select multiple dates on <b>Mac</b>: </p>")),
-                    div("1. Press `cmd` + select the dates"),
-                    width = 3
                 ),
-                ui.panel_main(
-                    ui.output_plot("interactive")
-                ),
-            ),
+            ui.nav("Insights", "Statistical Insights in Construction!"),
+        ),
+            
         ),  
     ui.nav("Recommendations", 
            "In Construction! ML Predictions on the way!"),
