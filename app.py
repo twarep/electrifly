210d4faf49ae4d48ed88c7827ecadc45e5921e79
twarep <ptwareck@uwaterloo.ca>
2024-02-21 01:29:48 -0500

    updated padding and margins

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ app_ui = ui.page_fluid(
             ),
           ),
           div(HTML(f"""
-                    <div style="display: flex; align-items: center; justify-content: center; padding-bottom: 1rem">
+                    <div style="display: flex; align-items: center; justify-content: center; padding-bottom: 1rem; padding-top: 1rem;">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="width: 50px; height: auto; margin-right: 15px;">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M308.5 135.3c7.1-6.3 9.9-16.2 6.2-25c-2.3-5.3-4.8-10.5-7.6-15.5L304 89.4c-3-5-6.3-9.9-9.8-14.6c-5.7-7.6-15.7-10.1-24.7-7.1l-28.2 9.3c-10.7-8.8-23-16-36.2-20.9L199 27.1c-1.9-9.3-9.1-16.7-18.5-17.8C173.9 8.4 167.2 8 160.4 8h-.7c-6.8 0-13.5 .4-20.1 1.2c-9.4 1.1-16.6 8.6-18.5 17.8L115 56.1c-13.3 5-25.5 12.1-36.2 20.9L50.5 67.8c-9-3-19-.5-24.7 7.1c-3.5 4.7-6.8 9.6-9.9 14.6l-3 5.3c-2.8 5-5.3 10.2-7.6 15.6c-3.7 8.7-.9 18.6 6.2 25l22.2 19.8C32.6 161.9 32 168.9 32 176s.6 14.1 1.7 20.9L11.5 216.7c-7.1 6.3-9.9 16.2-6.2 25c2.3 5.3 4.8 10.5 7.6 15.6l3 5.2c3 5.1 6.3 9.9 9.9 14.6c5.7 7.6 15.7 10.1 24.7 7.1l28.2-9.3c10.7 8.8 23 16 36.2 20.9l6.1 29.1c1.9 9.3 9.1 16.7 18.5 17.8c6.7 .8 13.5 1.2 20.4 1.2s13.7-.4 20.4-1.2c9.4-1.1 16.6-8.6 18.5-17.8l6.1-29.1c13.3-5 25.5-12.1 36.2-20.9l28.2 9.3c9 3 19 .5 24.7-7.1c3.5-4.7 6.8-9.5 9.8-14.6l3.1-5.4c2.8-5 5.3-10.2 7.6-15.5c3.7-8.7 .9-18.6-6.2-25l-22.2-19.8c1.1-6.8 1.7-13.8 1.7-20.9s-.6-14.1-1.7-20.9l22.2-19.8zM112 176a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM504.7 500.5c6.3 7.1 16.2 9.9 25 6.2c5.3-2.3 10.5-4.8 15.5-7.6l5.4-3.1c5-3 9.9-6.3 14.6-9.8c7.6-5.7 10.1-15.7 7.1-24.7l-9.3-28.2c8.8-10.7 16-23 20.9-36.2l29.1-6.1c9.3-1.9 16.7-9.1 17.8-18.5c.8-6.7 1.2-13.5 1.2-20.4s-.4-13.7-1.2-20.4c-1.1-9.4-8.6-16.6-17.8-18.5L583.9 307c-5-13.3-12.1-25.5-20.9-36.2l9.3-28.2c3-9 .5-19-7.1-24.7c-4.7-3.5-9.6-6.8-14.6-9.9l-5.3-3c-5-2.8-10.2-5.3-15.6-7.6c-8.7-3.7-18.6-.9-25 6.2l-19.8 22.2c-6.8-1.1-13.8-1.7-20.9-1.7s-14.1 .6-20.9 1.7l-19.8-22.2c-6.3-7.1-16.2-9.9-25-6.2c-5.3 2.3-10.5 4.8-15.6 7.6l-5.2 3c-5.1 3-9.9 6.3-14.6 9.9c-7.6 5.7-10.1 15.7-7.1 24.7l9.3 28.2c-8.8 10.7-16 23-20.9 36.2L315.1 313c-9.3 1.9-16.7 9.1-17.8 18.5c-.8 6.7-1.2 13.5-1.2 20.4s.4 13.7 1.2 20.4c1.1 9.4 8.6 16.6 17.8 18.5l29.1 6.1c5 13.3 12.1 25.5 20.9 36.2l-9.3 28.2c-3 9-.5 19 7.1 24.7c4.7 3.5 9.5 6.8 14.6 9.8l5.4 3.1c5 2.8 10.2 5.3 15.5 7.6c8.7 3.7 18.6 .9 25-6.2l19.8-22.2c6.8 1.1 13.8 1.7 20.9 1.7s14.1-.6 20.9-1.7l19.8 22.2zM464 304a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
@@ -230,7 +230,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Data Visualizations</p>
-                                      <p>Gain comprehensive insights into plane and battery behaviours</p>
+                                      <p style="padding: 0 8rem;">Gain comprehensive insights into plane and battery behaviours</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -239,7 +239,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Custom Graphs</p>
-                                      <p>Develop your own graphs and plot any variable including flight and weather data</p>
+                                      <p style="padding: 0 8rem;">Develop your own graphs and plot any variable including flight and weather data</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -253,7 +253,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Data Preview</p>
-                                      <p>Preview data at a glance of the most recent flight to ensure all data was uploaded correctly</p>
+                                      <p style="padding: 0 8rem;">Preview data at a glance of the most recent flight to ensure all data was uploaded correctly</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -262,7 +262,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Statistical Insights</p>
-                                      <p>Unlock valuable statistical insights to enhance flight operations efficiency</p>
+                                      <p style="padding: 0 8rem;">Unlock valuable statistical insights to enhance flight operations efficiency</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -283,7 +283,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Flight Schedule</p>
-                                      <p>Determine the optimal flight times based on a simulation model derived from local weather forecasts</p>
+                                      <p style="padding: 0 8rem;">Determine the optimal flight times based on a simulation model derived from local weather forecasts</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -292,7 +292,7 @@ app_ui = ui.page_fluid(
                         ui.div(HTML(f"""
                                       <br>
                                       <p style="font-weight: bold">Flight Exercise Planning</p>
-                                      <p>Empower pilots’ confidence during flight by planning what exercises can be performed in the air</p>
+                                      <p style="padding: 0 8rem;">Empower pilots’ confidence during flight by planning what exercises can be performed in the air</p>
                                     """)),
                         style="display: flex; flex-direction: column; align-items: center; text-align: center;"
                         ),
@@ -300,7 +300,7 @@ app_ui = ui.page_fluid(
             ),
           ),
           div(HTML(f"""
-                    <div style="display: flex; align-items: center; justify-content: center; padding-bottom: 1rem">
+                    <div style="display: flex; align-items: center; justify-content: center; padding-top: 4rem; padding-bottom: 1rem">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="width: 50px; height: auto; margin-right: 15px;">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                           <path d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z"/>
